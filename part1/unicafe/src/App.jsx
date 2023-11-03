88bd61217a19bbd4c39bb833d1d1b60ef08a053c
Ranger Lee <mrrangerlee@gmail.com>
2023-11-03 16:30:28 -0700

    finished up to unicafe step4

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,34 +1,66 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const Header = (props) => {
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>{props.header}</h1>
     </>
+  )
+}
+
+const Button = (props) => {
+  return (
+    <>
+      <button onClick={props.handleClick}>{props.text}</button>
+    </>
+  )
+}
+
+const Text = (props) => {
+  return (
+  <>
+    <p>{props.text} {props.number}</p>
+  </>
+  )
+}
+
+const Statistics = (props) => {
+  if (props.good === 0 && props.neutral === 0 && props.bad === 0) {
+    return (
+      <>
+        No feedback given
+      </>
+    )
+  }
+  return (
+    <>
+      <Text text='good' number={props.good} />
+      <Text text='neutral' number={props.neutral} />
+      <Text text='bad' number={props.bad} />
+      <Text text='all' number={props.good + props.neutral + props.bad} />
+      <Text text='average' number={(props.good - props.bad) / (props.good + props.neutral + props.bad)} />
+      <Text text='positive' number={props.good / (props.good + props.neutral + props.bad)} />
+    </>
+  )
+}
+
+const App = () => {
+  const header = 'give feedback'
+  const statistics = 'statistics'
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  return (
+    <div>
+      <Header header={header} />
+      <Button handleClick={() => setGood(good + 1)} text='good' />
+      <Button handleClick={() => setNeutral(neutral + 1)} text='neutral' />
+      <Button handleClick={() => setBad(bad + 1)} text='bad' />
+      <Header header={statistics} />
+      <Statistics good={good} neutral={neutral} bad={bad} />
+    </div>
   )
 }
 
