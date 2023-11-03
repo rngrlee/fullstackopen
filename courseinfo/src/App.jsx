da14c01d4ab4db74ede9da5fa96f561f144aa6ed
Ranger Lee <mrrangerlee@gmail.com>
2023-11-03 15:40:54 -0700

    changed child name in Part

--- courseinfo/src/App.jsx
+++ courseinfo/src/App.jsx
@@ -1,4 +1,5 @@
 const Header = (props) => {
+  console.log(props)
   return (
     <>
       <h1>{props.course}</h1>
@@ -7,21 +8,24 @@ const Header = (props) => {
 }
 
 const Part = (props) => {
+  console.log(props)
   return (
-    <p>{props.part} {props.exercises}</p>
+    <p>{props.name} {props.exercises}</p>
   )
 }
 const Content = (props) => {
+  console.log(props)
   return (
     <>
-      <Part part={props.parts[0].name} exercises={props.parts[0].exercises}/>
-      <Part part={props.parts[1].name} exercises={props.parts[1].exercises}/>
-      <Part part={props.parts[2].name} exercises={props.parts[2].exercises}/>
+      <Part name={props.parts[1].name} exercises={props.parts[1].exercises}/>
+      <Part name={props.parts[2].name} exercises={props.parts[2].exercises}/>
+      <Part name={props.parts[0].name} exercises={props.parts[0].exercises}/>
     </>
   )
 }
 
 const Total = (props) => {
+  console.log(props)
   return (
     <>
       <p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises} </p>
