73154a68dd978f3856b33e4b59ee236650349d85
Ranger Lee <mrrangerlee@gmail.com>
2023-11-09 23:29:10 -0800

    finished phonebook backend step 8 (3.8*)

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,8 +1,11 @@
 const express = require('express')
+const morgan = require('morgan')
 
 const app = express()
 
 app.use(express.json())
+morgan.token('data', (request) => JSON.stringify(request.body))
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :data'))
 
 const date = new Date()
 const toDateString = date.toDateString()
