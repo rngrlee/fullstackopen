309b179624f792c52d5faccf47273e7bfb41dbfd
Ranger Lee <mrrangerlee@gmail.com>
2023-12-30 18:16:27 -0800

    added hideable expanded view of blogs (5.7)

--- part5/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,37 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const [visible, setVisible] = useState(false)
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const viewedBlog = () => {
+    return (
+      <>
+        {blog.url}<br />
+        likes: {blog.likes} <button>like</button><br />
+        {blog.user.name}
+      </>
+    )
+  }
+
+  return (
+    <div style={blogStyle}>
+      <div>
+        {blog.title} {blog.author}
+        <button onClick={() => setVisible(!visible)}>
+          {visible ? 'hide' : 'view'}
+        </button>
+      </div>
+      {visible && viewedBlog()}
+    </div>  
+  )
+}
 
 export default Blog
