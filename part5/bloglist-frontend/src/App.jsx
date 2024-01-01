42a6ab3147a7fe25c541de10178c4e78fcd0c5f7
Ranger Lee <mrrangerlee@gmail.com>
2024-01-01 00:47:23 -0800

    sorted by likes (5.10)

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -128,7 +128,7 @@ const App = () => {
           <BlogForm createBlog={handleCreate}></BlogForm>
         </Togglable>
 
-        {blogs.map(blog =>
+        {blogs.sort((a, b) => b.likes - a.likes).map(blog =>
             <Blog key={blog.id} blog={blog} updateBlog={handleLike}/>
           )}
       </>
